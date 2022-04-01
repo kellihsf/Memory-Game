6cdbdf35341410e009e0391d04671dfac51c633e
Kelli <kellistone217@gmail.com>
2022-03-31 20:42:28 -0600

    Removed boiler plate code and logo, created Memory Card component, created memory cards, added card design

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './MemoryCard.css';
+
+class MemoryCard extends React.Component {
+    render() {
+        return (
+            <div className="MemoryCard">
+                <img className="image" src="https://media.istockphoto.com/vectors/mountain-landscape-cartoon-mountain-landscape-with-green-meadows-and-vector-id1070540902?k=20&m=1070540902&s=170667a&w=0&h=JuOwuUtymnI8Mr1ECdMXDzgg1qi_CKkXtIbdzIYBLow=">
+                </img>
+            </div>
+        ) 
+    }
+} 
+
+export default MemoryCard;
